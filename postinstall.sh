984a0c29e59a1eb861174e842a0c1599ef86903c
Jared Meeker <jared@meekers.org>
2020-01-30 22:39:20 -0700

    added xinitrc

--- postinstall.sh
+++ postinstall.sh
@@ -29,6 +29,7 @@ fi
 cp -f dotfiles/.tmux.conf /home/$INSTALLUSER/
 mkdir -p /home/$INSTALLUSER/.config
 cp -af dotfiles/fish /home/$INSTALLUSER/.config/
+cp -f dotfiles/.xinitrc /home/$INSTALLUSER/
 
 # dotfile - fix perms
-chown -R $INSTALLUSER:$INSTALLUSER /home/$INSTALLUSER/.config
+chown -R $INSTALLUSER:$INSTALLUSER /home/$INSTALLUSER
